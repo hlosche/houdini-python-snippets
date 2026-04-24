2a57868775cd3e8e31da642ec039b786729f34c3
Hans Lösche <58704289+hlosche@users.noreply.github.com>
2026-04-24 08:10:33 -0400

    Implement selected tag menu functionality

---
+++ selected_tag_menu.py
@@ -0,0 +1,18 @@
+import crowdstoolutils
+
+node   = kwargs["node"]
+inputs = node.inputs()
+if not inputs or inputs[0] is None:
+    return []
+
+geo = inputs[0].geometry()
+if not geo or not geo.points():
+    return []
+
+info = geo.points()[0].attribValue("fbuild_agent_info")
+if not info:
+    return []
+
+pca  = info.get("pca", {})
+tags = sorted(pca.keys())
+return crowdstoolutils.buildMenuStringList(tags)
