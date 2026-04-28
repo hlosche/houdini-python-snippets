9b030c797bb8fd99766084bd43ee5e361f0c6438
Hans Lösche <58704289+hlosche@users.noreply.github.com>
2026-04-28 07:33:29 -0400

    Refactor detail calls to point for channel data

--- pca_variation_lookup.cpp
+++ pca_variation_lookup.cpp
@@ -1,13 +1,14 @@
-string ch_names[]   = detail(0, "pca_channel_names", 0);                                                                         
-float  weights[]    = detail(0, "pca_variation_weights", 0);                                                                     
-int    n            = detail(0, "pca_num_channels", 0);                                                                          
-string tag_layers[] = detail(0, "pca_tag_layers", 0);                                                                            
+string ch_names[]   = point(0, "pca_channel_names",     @primnum);
+float  weights[]    = point(0, "pca_variation_weights", @primnum);
+string tag_layers[] = point(0, "pca_tag_layers",        @primnum);
+
+int n = len(ch_names);
 
 for (int i = 0; i < n; i++)
 {
-  int ch_idx = agentrigfindchannel(0, @primnum, ch_names[i]);
-  if (ch_idx >= 0)
-      setagentchannelvalue(0, @primnum, weights[i], ch_idx);
+    int ch_idx = agentrigfindchannel(0, @primnum, ch_names[i]);
+    if (ch_idx >= 0)
+        setagentchannelvalue(0, @primnum, weights[i], ch_idx);
 }
 
 setagentcurrentlayers(0, @primnum, tag_layers);
